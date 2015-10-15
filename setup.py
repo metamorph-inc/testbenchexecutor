ef196c5e1837354d8b523aa4c06826a8bb1eda36
Kevin Smyth <kevin.m.smyth@gmail.com>
2015-10-15 10:44:07 -0500

    testbenchexecutor: runs a testbench_manifest.json

---
+++ setup.py
@@ -0,0 +1,22 @@
+'''
+Build with:
+setup.py bdist_wheel
+or install in editable mode with
+Scripts\pip install -e path\to\testbenchexecutor
+'''
+from setuptools import setup
+
+setup(
+    name='testbenchexecutor',
+    version='0.1.1',
+    author='MetaMorph Software, Inc',
+    author_email='adam@metamorphsoftware.com',
+    description='Executes the steps in a Test Bench manifest',
+    packages=['testbenchexecutor'],
+    package_dir={'testbenchexecutor': 'testbenchexecutor'},
+    entry_points = {
+        "console_scripts": [
+            "testbenchexecutor = testbenchexecutor.__main__:main",
+        ]
+    }
+)
