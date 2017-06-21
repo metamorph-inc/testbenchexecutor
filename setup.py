5bf466f478652bf798a4e155c9a7946fdbf7b8a1
Kevin Smyth <kevin.m.smyth@gmail.com>
2017-06-21 18:07:57 -0500

    Note install_requires

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ from setuptools import setup
 
 setup(
     name='testbenchexecutor',
-    version='0.1.3',
+    version='0.1.4',
     author='MetaMorph Software, Inc',
     author_email='adam@metamorphsoftware.com',
     description='Executes the steps in a Test Bench manifest',
     packages=['testbenchexecutor'],
     package_dir={'testbenchexecutor': 'testbenchexecutor'},
+    install_requires=['python-dateutil'],
     entry_points = {
         "console_scripts": [
             "testbenchexecutor = testbenchexecutor.__main__:main",
