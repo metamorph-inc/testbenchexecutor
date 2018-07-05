bfdc63645ccb528489b52ca2736dcd333d1ead78
Kevin Smyth <kevin.m.smyth@gmail.com>
2018-07-05 10:07:32 -0500

    v0.1.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='testbenchexecutor',
-    version='0.1.4',
+    version='0.1.5',
     author='MetaMorph Software, Inc',
     author_email='adam@metamorphsoftware.com',
     description='Executes the steps in a Test Bench manifest',
